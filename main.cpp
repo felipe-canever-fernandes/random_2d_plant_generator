13f3412cdde69699c0ae3ac391e806af69e6adc9
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-07-05 21:50:37 -0300

    Include SFML

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include <print>
+#include <SFML/Graphics.hpp>
 
 auto main() -> int
 {
-   std::println("Hello, Random 2D Plant Generator!");
+   auto window = sf::RenderWindow
+   (
+      sf::VideoMode(800, 600),
+      "Random 2D Plant Generator"
+   );
+
+   auto const rectangle = []
+   {
+      auto rectangle =  sf::RectangleShape({100, 300});
+      rectangle.setFillColor(sf::Color::Green);
+      return rectangle;
+   }();
+
+   while (window.isOpen())
+   {
+      for (auto event = sf::Event(); window.pollEvent(event);)
+         if (event.type == sf::Event::Closed)
+            window.close();
+
+      window.clear();
+      window.draw(rectangle);
+      window.display();
+   }
 }
