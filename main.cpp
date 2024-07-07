6c75c01a63bc4c72f40f273a73efbfdafc581ddc
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-07-06 22:46:38 -0300

    Make window size relative to screen size

--- main.cpp
+++ main.cpp
@@ -4,9 +4,17 @@ import random_2d_plant_generator;
 
 auto main() -> int
 {
+   auto const desktop_mode = sf::VideoMode::getDesktopMode();
+   constexpr auto window_size_ratio = 0.8;
+
    auto window = sf::RenderWindow
    (
-      sf::VideoMode(800, 600),
+      sf::VideoMode
+      (
+         static_cast<unsigned int>(desktop_mode.width * window_size_ratio),
+         static_cast<unsigned int>(desktop_mode.height * window_size_ratio)
+      ),
+
       "Random 2D Plant Generator"
    );
 
